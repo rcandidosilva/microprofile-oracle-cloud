7642fc53659f0df6a1ea0a7e871997319260c466
Rodrigo Cândido da Silva <rcandidosilva@gmail.com>
2018-11-17 07:18:07 -0200

    Initial commit

---
+++ src/main/java/com/example/microprofile/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.example.microprofile.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
